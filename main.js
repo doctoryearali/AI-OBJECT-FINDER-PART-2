3aed5202900a2b8f75816cc661a3ac0c33b57623
doctoryearali <82830552+doctoryearali@users.noreply.github.com>
2022-06-10 20:55:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+status=""
+
+function setup()
+{
+ canvas=createCanvas(400,400);
+ canvas.center();
+ video=createCapture(VIDEO);
+ video.size(400,400);
+ video.hide(); 
+}
+
+function start()
+{
+ objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+ document.getElementById("status").innerHTML="status:Detecting Object";
+ objname=document.getElementById("obj name").value;
+}
+
+function modelLoaded()
+{
+ console.log("Model is activated");
+ status=true;
+}
+
+function draw()
+{
+ image(video,0,0,400,400);
+}
